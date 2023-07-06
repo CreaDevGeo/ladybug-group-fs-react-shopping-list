ad6bda2331d6a682cda1fd1c4308668af743655e
Sam McCauley <sammccauley34@gmail.com>
2023-07-06 14:29:58 -0500

    Created Item.jsx and put in some basic logic

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react;'
+
+function Item(props) {
+
+    const [ purchased, setPurchased ] = useState(false);
+
+    const handlePurchase = () => {
+        console.log('props.item.name', purchased);
+    }
+
+    return (
+
+
+    )
+}
